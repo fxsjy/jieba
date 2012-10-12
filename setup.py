3c9ddd4da68dd150900b16950ea247edd2d7d904
Sun Junyi <ccnusjy@gmail.com>
2012-10-12 09:59:31 +0800

    version chage

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup  
 setup(name='jieba',  
-      version='0.16',  
+      version='0.17',  
       description='Chinese Words Segementation Utilities',  
       author='Sun, Junyi',  
       author_email='ccnusjy@gmail.com',  

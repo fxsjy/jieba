6c0bcf655759c584d90aad74d9efff1c8b775a6e
Sun Junyi <ccnusjy@gmail.com>
2012-11-06 10:33:09 +0800

    version change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup  
 setup(name='jieba',  
-      version='0.19',  
+      version='0.20',  
       description='Chinese Words Segementation Utilities',  
       author='Sun, Junyi',  
       author_email='ccnusjy@gmail.com',  

7ce3433316ae33e6eb35a88ba44204fa2759306b
Sun Junyi <ccnusjy@gmail.com>
2013-04-07 22:55:06 +0800

    fix bug: python2.6 does not support CRLF in eval(astring)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup  
 setup(name='jieba',  
-      version='0.26',  
+      version='0.26.1',  
       description='Chinese Words Segementation Utilities',  
       author='Sun, Junyi',  
       author_email='ccnusjy@gmail.com',  

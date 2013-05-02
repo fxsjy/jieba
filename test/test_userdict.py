c1bf81534388f5990c6f036a1f12a7302898ba18
Sun Junyi <ccnusjy@gmail.com>
2013-05-02 17:01:16 +0800

    update test case

--- test/test_userdict.py
+++ test/test_userdict.py
@@ -5,7 +5,8 @@ import jieba
 jieba.load_userdict("userdict.txt")
 import jieba.posseg as pseg
 
-test_sent = "李小福是创新办主任也是云计算方面的专家"
+test_sent = "李小福是创新办主任也是云计算方面的专家;"
+test_sent += "例如我输入一个带“韩玉赏鉴”的标题，在自定义词库中也增加了此词为N类型"
 words = jieba.cut(test_sent)
 for w in words:
 	print w

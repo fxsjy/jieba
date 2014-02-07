cc708de40ce7f65b0bee7192170a42353fb348a2
fxsjy <ccnusjy@gmail.com>
2014-02-07 15:22:53 +0800

    version 0.32 released

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup  
 setup(name='jieba',  
-      version='0.31',  
+      version='0.32',  
       description='Chinese Words Segementation Utilities',  
       author='Sun, Junyi',  
       author_email='ccnusjy@gmail.com',  

14671d4feb22844c0839eeedf4fe7f8d43c501d9
Dingyuan Wang <abcdoyle888@gmail.com>
2014-10-19 10:41:09 +0800

    fix __main__.py

--- jieba/__main__.py
+++ jieba/__main__.py
@@ -3,7 +3,7 @@ import sys
 import jieba
 from argparse import ArgumentParser
 
-parser = ArgumentParser(usage="%s -m jieba [options] filename" % sys.executable, description="Jieba command line interface.", version="Jieba " + jieba.__version__, epilog="If no filename specified, use STDIN instead.")
+parser = ArgumentParser(usage="%s -m jieba [options] filename" % sys.executable, description="Jieba command line interface.", epilog="If no filename specified, use STDIN instead.")
 parser.add_argument("-d", "--delimiter", metavar="DELIM", default=' / ',
                     nargs='?', const=' ',
                     help="use DELIM instead of ' / ' for word delimiter; use a space if it is without DELIM")
@@ -14,6 +14,7 @@ parser.add_argument("-n", "--no-hmm", dest="hmm", action="store_false",
                     default=True, help="don't use the Hidden Markov Model")
 parser.add_argument("-q", "--quiet", action="store_true", default=False,
                     help="don't print loading messages to stderr")
+parser.add_argument("-V", '--version', action='version', version="Jieba " + jieba.__version__)
 parser.add_argument("filename", nargs='?', help="input file")
 
 args = parser.parse_args()
@@ -29,7 +30,7 @@ jieba.initialize()
 ln = fp.readline()
 while ln:
 	l = ln.rstrip('\r\n')
-	print(delim.join(jieba.cut(ln.rstrip('\r\n'), cutall, hmm)).encode('utf-8'))
+	print(delim.join(jieba.cut(ln.rstrip('\r\n'), cutall, hmm)))
 	ln = fp.readline()
 
 fp.close()

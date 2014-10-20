17437ae1505d5f28f8573802565905fae9968d70
Fukuball Lin <fukuball@gmail.com>
2014-10-20 16:24:04 +0800

    增加 extract_tags_with_weight.py 測試

--- test/extract_tags_with_weight.py
+++ test/extract_tags_with_weight.py
@@ -5,7 +5,7 @@ import jieba
 import jieba.analyse
 from optparse import OptionParser
 
-USAGE = "usage:    python extract_tags_with_weight.py [file name] -k [top k] -w [with weight=True or False]"
+USAGE = "usage:    python extract_tags_with_weight.py [file name] -k [top k] -w [with weight=1 or 0]"
 
 parser = OptionParser(USAGE)
 parser.add_option("-k", dest="topK")
@@ -24,13 +24,20 @@ if opt.topK is None:
 else:
     topK = int(opt.topK)
 
-if opt.withWeight is True:
-    withWeight = True
-else:
+if opt.withWeight is None:
     withWeight = False
+else:
+    if int(opt.withWeight) is 1:
+        withWeight = True
+    else:
+        withWeight = False
 
 content = open(file_name, 'rb').read()
 
 tags = jieba.analyse.extract_tags(content, topK=topK, withWeight=withWeight)
 
-print ",".join(tags)
+if withWeight is True:
+    for tag in tags:
+        print "tag: %s\t\t weight: %f" % (tag[1],tag[0])
+else:
+    print ",".join(tags)

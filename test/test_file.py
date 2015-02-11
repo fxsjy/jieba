8cbb26a7b674c6828ead6e9c8fc7e959e4033adf
fxsjy <ccnusjy@gmail.com>
2015-02-11 16:47:57 +0800

    fix test_file.py

--- test/test_file.py
+++ test/test_file.py
@@ -16,6 +16,6 @@ log_f = open("1.log","wb")
 log_f.write(words.encode('utf-8'))
 log_f.close()
 
-print('cost ' + tm_cost)
+print('cost ' + str(tm_cost))
 print('speed %s bytes/second' % (len(content)/tm_cost))
 

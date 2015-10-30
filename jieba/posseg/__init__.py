e5e41a4aad965b36da2c7d566c3d1734465cb419
jerryday <jerryday@yeah.net>
2015-10-30 16:38:50 +0800

    fix pair object in dict problem

--- jieba/posseg/__init__.py
+++ jieba/posseg/__init__.py
@@ -84,6 +84,12 @@ class pair(object):
     def __lt__(self, other):
         return self.word < other.word
 
+    def __eq__(self, other):
+        return isinstance(other, pair) and self.word == other.word and self.flag == other.flag
+
+    def __hash__(self):
+        return hash(self.word)
+
     def encode(self, arg):
         return self.__unicode__().encode(arg)
 

398db308ed15fe0198de7399b23ad0f15aa75120
nnl93 <liunannan.bfsumun@gmail.com>
2018-08-21 17:46:44 +0800

    Update test_userdict.py

--- test/test_userdict.py
+++ test/test_userdict.py
@@ -8,10 +8,11 @@ import jieba.posseg as pseg
 
 jieba.add_word('石墨烯')
 jieba.add_word('凱特琳')
+jieba.add_word('女士')
 jieba.del_word('自定义词')
 
 test_sent = (
-"李小福是创新办主任也是云计算方面的专家; 什么是八一双鹿\n"
+"李小福女士是创新办主任也是云计算方面的专家; 什么是八一双鹿\n"
 "例如我输入一个带“韩玉赏鉴”的标题，在自定义词库中也增加了此词为N类\n"
 "「台中」正確應該不會被切開。mac上可分出「石墨烯」；此時又可以分出來凱特琳了。"
 )

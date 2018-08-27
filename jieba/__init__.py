36a27302ce345a1866d125a9e59bd8611cf06813
CY Wang <a0953218488@gmail.com>
2018-08-27 17:05:46 +0800

    Fix  __init__ "-" symbol issue
    Solving "-" symbol can't be analyze issue . 
    
    For example,
    In keyword , chap-EX喬沛詩 , SK-II  ...etc 
    the present version will show "chap", "-", "EX喬沛詩" , "SK", "-", "II"
    
    After the modify,
    The new version will show  "chap-EX","喬沛詩" , "SK-II" 
    
    ps: I have used the jieba.load_userdict() , and added  "chap-EX" , "喬沛詩", "SK-II" in the userdict.txt.

--- jieba/__init__.py
+++ jieba/__init__.py
@@ -40,7 +40,10 @@ re_eng = re.compile('[a-zA-Z0-9]', re.U)
 
 # \u4E00-\u9FD5a-zA-Z0-9+#&\._ : All non-space characters. Will be handled with re_han
 # \r\n|\s : whitespace characters. Will not be handled.
-re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\._%]+)", re.U)
+# re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\._%]+)", re.U)
+# Adding "-" symbol in re_han_default
+re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\._%\-]+)", re.U)
+
 re_skip_default = re.compile("(\r\n|\s)", re.U)
 re_han_cut_all = re.compile("([\u4E00-\u9FD5]+)", re.U)
 re_skip_cut_all = re.compile("[^a-zA-Z0-9+#\n]", re.U)

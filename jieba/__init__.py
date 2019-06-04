b898efb775acd81874d4cde282a849e627bc53e2
zhuqisi <qszhu@fiberhome.com>
2019-06-04 11:37:27 +0800

    sentence split exclude · many names include ·

--- jieba/__init__.py
+++ jieba/__init__.py
@@ -42,7 +42,7 @@ re_eng = re.compile('[a-zA-Z0-9]', re.U)
 # \r\n|\s : whitespace characters. Will not be handled.
 # re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\._%]+)", re.U)
 # Adding "-" symbol in re_han_default
-re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\._%\-]+)", re.U)
+re_han_default = re.compile("([\u4E00-\u9FD5a-zA-Z0-9+#&\.·_%\-]+)", re.U)
 
 re_skip_default = re.compile("(\r\n|\s)", re.U)
 re_han_cut_all = re.compile("([\u4E00-\u9FD5]+)", re.U)

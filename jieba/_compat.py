284184139a414fd9da03e91fa2b8ad977d129997
root <root@bjyz-sys-gpu-kongming4.bjyz.baidu.com>
2019-12-11 07:00:20 +0000

    modify paddle install command

--- jieba/_compat.py
+++ jieba/_compat.py
@@ -72,7 +72,8 @@ def check_paddle_install():
         elif paddle.__version__ != '1.6.1':
             is_paddle_installed = False
             default_logger.debug("Check the paddle version is not correct, subject\
-            you to use command to install paddle: pip install paddlepaddle==1.6.1-tiny. Back to jieba basic cut......")
+            you to use command to install paddle: pip uninstall paddlepaddle(-gpu), \
+            pip install paddlepaddle==1.6.1-tiny. Now, back to jieba basic cut......")
     except ImportError:
         default_logger.debug("Can not import paddle, back to jieba basic cut......")
         is_paddle_installed = False

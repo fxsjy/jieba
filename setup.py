68ce6955b7e9a2af61ef03ab24a5f48425d46a01
fxsjy <ccnusjy@gmail.com>
2019-12-25 10:35:22 +0800

    update version to 0.40

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ GitHub: https://github.com/fxsjy/jieba
 """
 
 setup(name='jieba',
-      version='0.39',
+      version='0.40',
       description='Chinese Words Segmentation Utilities',
       long_description=LONGDOC,
       author='Sun, Junyi',
